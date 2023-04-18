3ae20771bd8de43e61e26de2bdc482a79495a421
Akshit soni <akshit.soni@teamwave.com>
2023-04-18 21:44:56 +0530

    android build gradle changes

--- lib/common/repository/auth_repository.dart
+++ lib/common/repository/auth_repository.dart
@@ -1,51 +1,26 @@
 import 'dart:convert';
 
-import 'package:auth0_flutter/auth0_flutter.dart';
-import 'package:blip_chat_app/common/constants.dart';
 import 'package:blip_chat_app/common/models/auth0_id_token.dart';
-import 'package:flutter_appauth/flutter_appauth.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthRepository {
-  final FlutterAppAuth _appAuth = const FlutterAppAuth();
-
   Auth0IdToken? idToken;
-  UserProfile? userProfile;
-  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
 
   Future<void> loginAction() async {
-    try {
-      AuthorizationTokenRequest authorizationTokenRequest =
-          AuthorizationTokenRequest(
-              Constants.AUTH0_CLIENT_ID, Constants.Redirect_Url,
-              issuer: Constants.AUTH0_ISSUER,
-              scopes: [
-            'openid',
-            'profile',
-            'email',
-            'offline_access',
-          ],
-              promptValues: [
-            'login'
-          ]);
-
-      AuthorizationTokenResponse? authorizationTokenResponse =
-          await _appAuth.authorizeAndExchangeCode(authorizationTokenRequest);
-    } catch (e) {
+    try {} catch (e) {
       rethrow;
     }
   }
 
-  _setLocalVariables(
-      {required AuthorizationTokenResponse? authorizationTokenResponse}) {
-    _secureStorage.write(
-        key: Constants.idTokenKey, value: authorizationTokenResponse?.idToken);
-    _secureStorage.write(
-        key: Constants.accessTokenKey,
-        value: authorizationTokenResponse?.accessToken);
+  // _setLocalVariables(
+  //     {required AuthorizationTokenResponse? authorizationTokenResponse}) {
+  //   _secureStorage.write(
+  //       key: Constants.idTokenKey, value: authorizationTokenResponse?.idToken);
+  //   _secureStorage.write(
+  //       key: Constants.accessTokenKey,
+  //       value: authorizationTokenResponse?.accessToken);
 
-    idToken = _parseIdToken(idToken: authorizationTokenResponse?.idToken ?? "");
-  }
+  //   idToken = _parseIdToken(idToken: authorizationTokenResponse?.idToken ?? "");
+  // }
 
   Auth0IdToken _parseIdToken({required String idToken}) {
     final parts = idToken.split(r'.');
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import flutter_appauth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
 }
