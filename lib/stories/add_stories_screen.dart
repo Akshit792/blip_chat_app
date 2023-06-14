6529f91b1e6ac08a8948d5d29de9ec41da127e0b
Akshit soni <hasknossit1234@gmail.com>
2023-06-14 21:14:23 +0530

    stories ui

--- lib/stories/add_stories_screen.dart
+++ lib/stories/add_stories_screen.dart
@@ -15,12 +15,18 @@ class _AddStoriesScreenState extends State<AddStoriesScreen>
     with WidgetsBindingObserver {
   CameraController? controller;
   bool _isCameraInitialized = false;
+  double _minAvailableZoom = 1.0;
+  double _maxAvailableZoom = 1.0;
+  double _currentZoomLevel = 1.0;
 
   @override
   void initState() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
     WidgetsBinding.instance.addObserver(this);
+
     onNewCameraSelected(cameras[0]);
+
     super.initState();
   }
 
@@ -53,9 +59,62 @@ class _AddStoriesScreenState extends State<AddStoriesScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: (_isCameraInitialized)
-          ? AspectRatio(
-              aspectRatio: (1 / controller!.value.aspectRatio),
-              child: controller!.buildPreview(),
+          ? Stack(
+              children: [
+                Container(
+                  color: ColorConstants.black,
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    width: double.infinity,
+                    child: controller!.buildPreview(),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(
+                    left: 10,
+                    top: MediaQuery.of(context).size.height * 0.76,
+                    right: 10,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Slider(
+                          value: _currentZoomLevel,
+                          min: _minAvailableZoom,
+                          max: _maxAvailableZoom,
+                          activeColor: Colors.white,
+                          inactiveColor: Colors.white30,
+                          onChanged: (value) async {
+                            setState(
+                              () {
+                                _currentZoomLevel = value;
+                              },
+                            );
+                            await controller!.setZoomLevel(value);
+                          },
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black87,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '${_currentZoomLevel.toStringAsFixed(1)}x',
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             )
           : const SizedBox.shrink(),
     );
@@ -80,14 +139,22 @@ class _AddStoriesScreenState extends State<AddStoriesScreen>
       );
     }
 
-    cameraController.addListener(
-      () {
-        if (mounted) setState(() {});
-      },
-    );
-
     try {
       await cameraController.initialize();
+
+      cameraController
+          .getMaxZoomLevel()
+          .then((value) => _maxAvailableZoom = value);
+
+      cameraController
+          .getMinZoomLevel()
+          .then((value) => _minAvailableZoom = value);
+
+      cameraController.addListener(
+        () {
+          if (mounted) setState(() {});
+        },
+      );
     } on CameraException catch (e, s) {
       LogPrint.error(
         error: e,
